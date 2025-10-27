ab69094bc7b14121f1deefb16d948ed98eaaab64
NightWarlock <edwincarrillopuentes8@gmail.com>
2025-10-27 10:06:46 -0600

    [INICIA] inicio proyecto de juego

---
+++ Juego_RA/app/src/main/java/com/example/juego_ra/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.juego_ra
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.juego_ra.ui.theme.Juego_RATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Juego_RATheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Juego_RATheme {
+    }
+}
